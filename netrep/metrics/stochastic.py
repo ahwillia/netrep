fix gaussian calculation

netrep/metrics/stochastic.py
@@ -6,7 +6,7 @@ class GaussianStochasticMetric:
     def __init__(self, group="orth"):
         self.group = group
 
-    def fit(self, X, Y, niter=10):
+    def fit(self, X, Y, niter=100):
         means_X, covs_X = X
         means_Y, covs_Y = Y
 
@@ -23,20 +23,22 @@ class GaussianStochasticMetric:
         sY = np.einsum("ijk,ik,ilk->ijl", uY, np.sqrt(vY), uY)
 
         T = align(means_Y, means_X, group=self.group)
-        Qs = [align(T @ sy, sx, group="orth") for sx, sy in zip(sX, sY)]
+        loss_hist = []
 
         for i in range(niter):
+            Qs = [align(T.T @ sy, sx, group="orth") for sx, sy in zip(sX, sY)]
             A = np.row_stack(
                 [means_X] + [sx for sx in sX]
             )
             r_sY = []
             B = np.row_stack(
-                [means_Y] + [Q @ sy for Q, sy in zip(Qs, sY)]
+                [means_Y] + [Q.T @ sy for Q, sy in zip(Qs, sY)]
             )
-            T = align(A, B, group=self.group)
+            T = align(B, A, group=self.group)
+            loss_hist.append(np.linalg.norm(A - B @ T))
 
         self.T = T
-        self.Qs = Qs
+        self.loss_hist = loss_hist
 
     def transform(self, X, Y):
         means_Y, covs_Y = Y
@@ -52,7 +54,12 @@ class GaussianStochasticMetric:
 
         A = np.sum((mX - mY) ** 2, axis=1)
         B = np.array([sq_bures_metric(sx, sy) for sx, sy in zip(sX, sY)])
-        return np.sqrt(np.mean(A + B))
+        mn = np.mean(A + B)
+        # mn should always be positive but sometimes numerical rounding errors
+        # cause mn to be very slightly negative, causing sqrt(mn) to be nan.
+        # Thus, we take sqrt(abs(mn)) and pass through the sign. Any large
+        # negative outputs should be caught by unit tests.
+        return np.sign(mn) * np.sqrt(abs(mn))
 
 
 

tests/test_stochastic.py
@@ -30,6 +30,32 @@ def test_gaussian_identity_covs(seed, m, n):
 
     # Fit model, assert distance == 0.
     metric = GaussianStochasticMetric(group="orth")
-    metric.fit(X, Y, niter=0)
+    metric.fit(X, Y)
+    assert abs(metric.score(X, Y)) < TOL
+
+
+@pytest.mark.parametrize('seed', [1, 2, 3])
+@pytest.mark.parametrize('m', [10])
+@pytest.mark.parametrize('n', [4])
+def test_gaussian_zero_means(seed, m, n):
+
+    # Set random seed, draw random rotation
+    rs = check_random_state(seed)
+    Q = rand_orth(n, n, random_state=rs)
+    Us = [rand_orth(n, n, random_state=np.random.RandomState(2)) for _ in range(m)]
+    Ps = [u @ np.diag(np.logspace(-1, 1, n)) @ u.T for u in Us]
+
+    # Create a pair of randomly rotated Gaussians.
+    mean_X = np.zeros((m, n))
+    mean_Y = np.zeros((m, n))
+    covs_X = np.array(Ps)
+    covs_Y = np.array([Q @ p @ Q.T for p in Ps])
+
+    X = (mean_X, covs_X)
+    Y = (mean_Y, covs_Y)
+
+    # Fit model, assert distance == 0.
+    metric = GaussianStochasticMetric(group="orth")
+    metric.fit(X, Y)
     assert abs(metric.score(X, Y)) < TOL
 