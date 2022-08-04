c2f375b6d104d8d181c4afb52a828f30fc447353
Alex Williams <alex.h.willia@gmail.com>
2022-08-04 14:05:38 -0400

    add final unit test for gaussian case

--- tests/test_stochastic.py
+++ tests/test_stochastic.py
@@ -49,7 +49,7 @@ def test_gaussian_zero_means(seed, m, n):
     mean_X = np.zeros((m, n))
     mean_Y = np.zeros((m, n))
     covs_X = np.array(Ps)
-    covs_Y = np.array([Q @ p @ Q.T for p in Ps])
+    covs_Y = np.array([Q.T @ p @ Q for p in Ps])
 
     X = (mean_X, covs_X)
     Y = (mean_Y, covs_Y)
@@ -59,3 +59,28 @@ def test_gaussian_zero_means(seed, m, n):
     metric.fit(X, Y)
     assert abs(metric.score(X, Y)) < TOL
 
+
+@pytest.mark.parametrize('seed', [1, 2, 3])
+@pytest.mark.parametrize('m', [10])
+@pytest.mark.parametrize('n', [4])
+def test_gaussian_general(seed, m, n):
+
+    # Set random seed, draw random rotation
+    rs = check_random_state(seed)
+    Q = rand_orth(n, n, random_state=rs)
+    Us = [rand_orth(n, n, random_state=np.random.RandomState(2)) for _ in range(m)]
+    Ps = [u @ np.diag(np.logspace(-1, 1, n)) @ u.T for u in Us]
+
+    # Create a pair of randomly rotated Gaussians.
+    mean_X = rs.randn(m, n)
+    mean_Y = mean_X @ Q
+    covs_X = np.array(Ps)
+    covs_Y = np.array([Q.T @ p @ Q for p in Ps])
+
+    X = (mean_X, covs_X)
+    Y = (mean_Y, covs_Y)
+
+    # Fit model, assert distance == 0.
+    metric = GaussianStochasticMetric(group="orth")
+    metric.fit(X, Y)
+    assert abs(metric.score(X, Y)) < TOL
