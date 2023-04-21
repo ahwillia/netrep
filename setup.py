033609c0eecce5d0f635db9113ba73ce0c0f9ea6
Alex Williams <alex.h.willia@gmail.com>
2023-04-21 18:03:53 -0400

    fix dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="netrep",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/ahwillia/netrep",
 
     author="Alex Williams",
@@ -11,4 +11,15 @@ setuptools.setup(
     description="Simple methods for comparing network representations.",
 
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy>=1.16.5',
+        'scipy>=1.3.1',
+        'scikit-learn>=0.21.3',
+        'tqdm>=4.32.2'
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=3.7'
+        ]
+    },
 )
