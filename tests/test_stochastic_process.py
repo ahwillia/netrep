5b88ec27e952b8a76c7a586a9304927b4a496195
Amin Nejatbakhsh <nejatbakhsh.amin@gmail.com>
2024-06-24 13:28:47 -0400

    Reimplemented tests using pytest

--- tests/test_stochastic_process.py
+++ tests/test_stochastic_process.py
@@ -2,6 +2,8 @@
 """
 Tests metrics betwen stochastic process neural representations.
 """
+
+import pytest
 import numpy as np
 from netrep.metrics import GPStochasticMetric,GaussianStochasticMetric
 from netrep.utils import rand_orth
@@ -9,7 +11,9 @@ from sklearn.utils.validation import check_random_state
 from sklearn.covariance import EmpiricalCovariance
 
 from numpy import random as rand
-from scipy.stats import ortho_group
+from netrep.utils import rand_orth
+
+TOL = 1e-6
 
 # %% Class for sampling from a gaussian process given a kernel
 class GaussianProcess:
@@ -31,71 +35,62 @@ class GaussianProcess:
         )
         return fs
 
-# %% Generating data from a GP given number of dimensions and time points
-n_dims = 3
-n_times = 10
-n_samples = 500
 
-ts = np.linspace(0,1,n_times)
-gpA = GaussianProcess(
-    kernel = lambda x, y: 1e-2*(1e-6*(x==y)+np.exp(-np.linalg.norm(x-y)**2/(2*1.**2))),
-    D=n_dims
-)
+# %%
+@pytest.mark.parametrize('seed', [1, 2, 3])
+@pytest.mark.parametrize('t', [10])  # number of time points
+@pytest.mark.parametrize('n', [4])   # number of neurons
+@pytest.mark.parametrize('k', [100]) # number of samples
+def test_gaussian_process(seed, t, n, k):
+    # Set random seed, draw random rotation
+    rs = check_random_state(seed)
+    Q = rand_orth(n, n, random_state=rs)
 
-sA = np.array([gpA.sample(ts) for _ in range(n_samples)])
-sAvec = sA.reshape(n_samples,n_dims*n_times)
-
-# %% Transforming the GP using an orthonormal matrix and computing SSD and DSSD
-Q = ortho_group.rvs(dim=n_dims)
-print('Determinant: ', np.linalg.det(Q))
-
-metric = GPStochasticMetric(n_dims=n_dims,group="orth")
-A = [sAvec.mean(0),EmpiricalCovariance().fit(sAvec).covariance_]
-
-B = [
-    np.kron(np.eye(n_times),Q)@A[0], 
-    np.kron(np.eye(n_times),Q)@A[1]@(np.kron(np.eye(n_times),Q)).T
-]
-
-metric.fit_score(A,B)
-# %% Computing SSD on the full NT*NT means and covariances
-metric = GaussianStochasticMetric(group="orth")
-
-A = [A[0][None],A[1][None]]
-B = [B[0][None],B[1][None]]
-
-metric.fit_score(A,B)
-# %% DSSD between GPs as a function of length scale
-lambdas = np.linspace(.1,1,5)
-dssds = np.zeros((len(lambdas),len(lambdas)))
-for i in range(len(lambdas)):
-    for j in range(len(lambdas)):
+    # Generate data from a gaussian process with RBF kernel
+    ts = np.linspace(0,1,t)
+    gpA = GaussianProcess(
+        kernel = lambda x, y: 1e-2*(1e-6*(x==y)+np.exp(-np.linalg.norm(x-y)**2/(2*1.**2))),
+        D=n
+    )
+    sA = np.array([gpA.sample(ts) for _ in range(k)]).reshape(k,n*t)
     
-        gpA = GaussianProcess(
-            kernel = lambda x, y: 1e-2*(1e-6*(x==y)+np.exp(-np.linalg.norm(x-y)**2/(2*lambdas[i]**2))),
-            D=n_dims
-        )
+    # Transform GP according to a rotation applied to individiual 
+    # blocks of the full covariance matrix
+    A = [sA.mean(0),EmpiricalCovariance().fit(sA).covariance_]
+    B = [
+        np.kron(np.eye(t),Q)@A[0], 
+        np.kron(np.eye(t),Q)@A[1]@(np.kron(np.eye(t),Q)).T
+    ]
+    
 
-        gpB = GaussianProcess(
-            kernel = lambda x, y: 1e-2*(1e-6*(x==y)+np.exp(-np.linalg.norm(x-y)**2/(2*lambdas[j]*2))),
-            D=n_dims
-        )
+    # Compute DSSD
+    metric = GPStochasticMetric(n_dims=n,group="orth")
 
-        sA = np.array([gpA.sample(ts) for _ in range(n_samples)]).reshape(n_samples,n_dims*n_times)
-        sB = np.array([gpB.sample(ts) for _ in range(n_samples)]).reshape(n_samples,n_dims*n_times)
+    dssd = metric.fit_score(A,B)
+    assert abs(dssd) < TOL
 
-        A = [sA.mean(0),EmpiricalCovariance().fit(sA).covariance_]
-        B = [sB.mean(0),EmpiricalCovariance().fit(sB).covariance_]
+    # Compute marginal SSD
+    metric = GaussianStochasticMetric(group="orth")
 
-        metric = GPStochasticMetric(n_dims=n_dims,group="orth")
-        dssds[i,j] = metric.fit_score(A,B)
+    A_marginal = [
+        A[0].reshape(t,n),
+        np.array([A[1][i*n:(i+1)*n,i*n:(i+1)*n] for i in range(t)])
+    ]
 
-# %% Plotting the result
-import matplotlib.pyplot as plt
-plt.imshow(dssds)
-plt.xlabel('$\lambda$')
-plt.ylabel('$\lambda$')
-plt.xticks(np.arange(len(lambdas)),lambdas)
-plt.yticks(np.arange(len(lambdas)),lambdas)
-plt.colorbar()
-plt.show()
+    B_marginal = [
+        B[0].reshape(t,n),
+        np.array([B[1][i*n:(i+1)*n,i*n:(i+1)*n] for i in range(t)])
+    ]
+
+    marginal_ssd = metric.fit_score(A_marginal,B_marginal)
+    assert abs(marginal_ssd) < TOL
+
+    # Compute full SSD
+    metric = GaussianStochasticMetric(group="orth")
+
+    A_full = [A[0][None],A[1][None]]
+    B_full = [B[0][None],B[1][None]]
+
+    full_ssd = metric.fit_score(A_full,B_full)
+
+    assert abs(full_ssd) > TOL
